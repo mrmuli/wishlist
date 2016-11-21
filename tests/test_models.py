9d1726e481b9b12b27a727df1d9d9442732f524f
andela-jmuli <joseph.muli@andela.com>
2016-11-21 16:54:20 +0300

    [Chore] Add test config data for CI Add tests

---
+++ tests/test_models.py
@@ -0,0 +1,33 @@
+from base_tests import BaseTest
+from rest_framework import status
+
+class TestUsers(BaseTest):
+
+    def test_successful_user_creation(self):
+        response = self.create_users(self.test_user1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_registration_without_username(self):
+        user = {'password': self.fake.password()}
+        response = self.create_users(user)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_registration_without_password(self):
+        user = {'username': self.fake.user_name()}
+        response = self.create_users(user)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login(self):
+        self.create_users(self.test_user2)
+        response = self.client.post('/auth/login/', self.test_user2)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_without_password(self):
+        user = {'username': self.fake.user_name()}
+        response = self.client.post('/auth/login/', user)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_without_username(self):
+        user = {'password': self.fake.password()}
+        response = self.client.post('/auth/login/', user)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
