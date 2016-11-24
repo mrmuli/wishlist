302ea32e1ef5c07912cc6a61505f70e2632cf9ea
andela-jmuli <joseph.muli@andela.com>
2016-11-24 16:54:48 +0300

    [Feature] Add permission classes for viewsets

--- api/viewsets.py
+++ api/viewsets.py
@@ -3,30 +3,44 @@ from serializers import BucketlistSerializer, BucketlistItemSerializer, UserSeri
 from rest_framework import generics, viewsets
 from django.contrib.auth.models import User
 from models import Bucketlist, BucketlistItem
+from rest_framework.permissions import IsAuthenticated
 
+class BaseMixin(object):
+    """
+    The class contained shared resources amongst viewsets
+    i.e. : permissions, filtering and authentication
+    """
+    permission_classes = (
+    'rest_framework.permissions.IsAuthenticated',
+    )
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class BucketlistView(generics.ListCreateAPIView):
+class BucketlistView(BaseMixin, generics.ListCreateAPIView):
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
 
     def get_queryset(self):
+        """
+        This view returns a list of all the bucketlists
+        for the currently authenticated user.
+        """
         return Bucketlist.objects.filter(created_by=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
+        user = self.request.user
+        serializer.save(created_by=user)
 
 
-class BucketlistDetail(generics.RetrieveUpdateDestroyAPIView):
+class BucketlistDetail(BaseMixin, generics.RetrieveUpdateDestroyAPIView):
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
 
 
-class BucketlisItemView(generics.CreateAPIView):
+class BucketlisItemView(BaseMixin, generics.CreateAPIView):
     queryset = BucketlistItem.objects.all()
     serializer_class = BucketlistItemSerializer
 
@@ -35,23 +49,23 @@ class BucketlisItemView(generics.CreateAPIView):
         return BucketlistItem.objects.filter(bucketlist=buck_id)
 
 
-class BucketlistItemUpdate(generics.UpdateAPIView):
+class BucketlistItemUpdate(BaseMixin, generics.UpdateAPIView):
     queryset = BucketlistItem.objects.all()
     serializer_class = BucketlistItemSerializer
 
     def get_queryset(self):
-        buck_id = self.kwargs['buck_id']
+        buck_id = self.kwargs['id']
         item_id = self.kwargs['pk']
         bucketlistitem = BucketlistItem.objects.filter(id=item_id, bucketlist=buck_id)
         return bucketlistitem
 
 
-class BucketlistItemDestroy(generics.DestroyAPIView):
+class BucketlistItemDestroy(BaseMixin, generics.DestroyAPIView):
     queryset = BucketlistItem.objects.all()
     serializer_class = BucketlistItemSerializer
 
     def get_queryset(self):
-        buck_id = self.kwargs['buck_id']
+        buck_id = self.kwargs['id']
         item_id = self.kwargs['pk']
         bucketlistitem = BucketlistItem.objects.filter(id=item_id, bucketlist=buck_id)
         return bucketlistitem
